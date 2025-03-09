7e12f102b2f556a4d67d30e78b10ab2eabc8e4e3
Nizhnikovsky <Nizhnikovsky89@gmail.com>
2025-03-09 17:26:03 +0100

    Structure fixes

--- src/Domain/Team/Model/Team.php
+++ src/Domain/Team/Model/Team.php
@@ -24,7 +24,7 @@ class Team
      */
     public function addPlayer(TeamPlayer $player): void
     {
-        if (count($this->players) >= 11) {
+        if (count($this->players) == 11) {
             throw new TeamAmountExceededException();
         }
 
